a83b95aaab0ed3506b84b64af84a5b3de0ac135e
Djebril <djebrilacademy@gmail.com>
2024-04-19 19:25:35 +0100

    add a small description and a picture and fiw the hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { Link } from "react-scroll";
 import "./App.css";
 
@@ -11,9 +9,9 @@ function App() {
     return (
       <div
         style={{ display: info ? "block" : "none" }}
-        className="bg-red-100 text-white"
+        className="bg-red-100 text-white rounded-tl-3xl rounded-tr-3xl animate__animated animate__fadeInDown "
       >
-        <ul className="flex flex-col justify-between items-center text-black-100  max-sm:flex-col indie-flower-regular gap-8">
+        <ul className="flex flex-col justify-between items-center text-black-100  max-sm:flex-col indie-flower-regular gap-8 p-4">
           <li className="px-4 font-semibold text-black cursor-pointer hover:underline text-xl capitalize">
             <Link to="home" spy={true} smooth={true} offset={50} duration={500}>
               home
@@ -63,32 +61,38 @@ function App() {
     <>
       <nav
         style={{ backgroundColor: "#FFFDCB" }}
-        className="shadow-lg  px-8 py-1 rounded-bl-3xl flex justify-between lg:justify-between items-center"
+        className="shadow-lg  px-16 py-1 rounded-bl-3xl flex justify-center lg:justify-between items-center w-full"
       >
-        <div className="flex justify-between items-center ">
-          <img
-            className="rounded-full h-24 w-24"
-            src="https://yt3.googleusercontent.com/GDpaxeA5Pzjb25OFU2yG1OZAfAgzKMzSVpqBAsU7SzZmMkDU2XvBGtIytkmLvLcGVfSPGGoJqg=s176-c-k-c0x00ffffff-no-rj"
-            alt=""
-          />
-         
-          <img
-            onClick={() => {
-              if (isListOn) {
-                setListOn(false);
-              } else {
-                setListOn(true);
+        <div className="flex items-center justify-between w-full">
+          <div className="">
+            <img
+              className="rounded-full h-24 w-full "
+              src="https://yt3.googleusercontent.com/GDpaxeA5Pzjb25OFU2yG1OZAfAgzKMzSVpqBAsU7SzZmMkDU2XvBGtIytkmLvLcGVfSPGGoJqg=s176-c-k-c0x00ffffff-no-rj"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              onClick={() => {
+                if (isListOn) {
+                  setListOn(false);
+                } else {
+                  setListOn(true);
+                }
+              }}
+              className="w-8 h-8 lg:hidden"
+              h
+              src={
+                isListOn
+                  ? "https://cdn-icons-png.flaticon.com/128/3917/3917759.png"
+                  : "https://cdn-icons-png.flaticon.com/128/3917/3917762.png"
               }
-            }}
-            className="w-8 h-8 lg:hidden"
-            src="https://cdn-icons-png.flaticon.com/128/3917/3917762.png"
-            alt=""
-          />
-         
+              alt=""
+            />
+          </div>
         </div>
 
-        <ul className="flex justify-between items-center text-black-100 hidden lg:flex indie-flower-regular gap-8">
-
+        <ul className="flex justify-between items-center text-black-100 hidden lg:flex indie-flower-regular gap-16">
           <li className="px-4 font-semibold text-black cursor-pointer hover:underline text-xl capitalize">
             <Link to="home" spy={true} smooth={true} offset={50} duration={500}>
               home
@@ -107,7 +111,7 @@ function App() {
             </Link>
           </li>
 
-          <li className="px-4 font-semibold text-black cursor-pointer hover:underline text-xl capitalize">
+          <li className="px-4 font-semibold text-black cursor-pointer hover:underline text-xl capitalize w-max">
             <Link
               to="scocial-media"
               spy={true}
@@ -115,8 +119,8 @@ function App() {
               offset={-100}
               duration={500}
             >
-              social media
-            </Link>
+             social media
+             </Link>
           </li>
 
           <li className="px-4 font-semibold text-black cursor-pointer hover:underline text-xl capitalize">
@@ -134,26 +138,26 @@ function App() {
       </nav>
       <List isOn={isListOn} />
 
-      <section id="home">
-        <div>
-          <h1>
-            {" "}
-            welcome to <br /> mohamed lbjaoui school
+      <section className="flex flex-col items-center justify-center lg:justify-between lg:flex-row mt-24 lg:mt-0 px-16 lg:py-12 gap-12 bg-cover bg-center h-screen home indie-flower-regular capitalize" id="home" src="">
+        <div className="lg:w-1/2 flex flex-col justify-center items-center lg:items-start text-center lg:text-left gap-12 lg:mb-64">
+          <h1 className="text-4xl capitalize">
+            welcome to  Mohamed Lbjaoui <br /> High School
           </h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consequuntur, suscipit, accusantium iste minus veniam esse quisquam
-            ipsum, porro incidunt blanditiis molestias beatae explicabo tenetur
-            placeat at consectetur facere. Sunt ipsam delectus impedit!
-            Assumenda nulla sint inventore nihil quas. Quaerat, ex?
+          <p className="text-xl">
+            Mohamed Lbjaoui High School is a beacon of educational excellence
+            nestled in the heart of Algiers, specifically in the bustling
+            neighborhood of Bab Ezzouar. Established with a vision to nurture
+            young minds and empower them for a bright future, our school stands
+            as a testament to academic rigor, holistic development, and
+            community engagement.
           </p>
         </div>
-        <div>
-          <img src="" alt="" />
+        <div className="lg:w-1/2 mb-64">
+          <img className="rounded-3xl" src="https://scontent.falg6-2.fna.fbcdn.net/v/t39.30808-6/210372420_4086465968115815_7468991513453658856_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEDlMLwxlHK02SKrBjOVVaXMGQgbaQisWswZCBtpCKxawU-oxxSRm7OuonzSl9nx3N4HCHgGLg72NrF1qcsKeVt&_nc_ohc=lQfTCQpiZBwAb4u3Okv&_nc_ht=scontent.falg6-2.fna&oh=00_AfB0u540Fl0jTUtXrO92p0YVJ7PDKKMqufI4bHRjZTv0ug&oe=662874E9" alt="" />
         </div>
       </section>
 
-      <section id="about">
+     <section id="about">
         <h1>our story</h1>
         <p></p>
       </section>
@@ -174,7 +178,7 @@ function App() {
 
       <div className="h-screen border-2" id="about">
         <h1>about</h1>
-      </div>
+      </div> 
     </>
   );
 }
