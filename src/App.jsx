8765e615030b0f837b9ec288ac9e97918c70ab22
Djebril <djebrilacademy@gmail.com>
2024-04-20 22:45:24 +0100

    we find everything good but go forward to the names edit commit

--- src/App.jsx
+++ src/App.jsx
@@ -156,45 +156,53 @@ function App() {
             as a testament to academic rigor, holistic development, and
             community engagement.
           </p>
-          
         </div>
         <div className="lg:w-1/2 mb-64">
           <img
-            className="rounded-3xl"
-            src="https://scontent.falg6-2.fna.fbcdn.net/v/t39.30808-6/210372420_4086465968115815_7468991513453658856_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEDlMLwxlHK02SKrBjOVVaXMGQgbaQisWswZCBtpCKxawU-oxxSRm7OuonzSl9nx3N4HCHgGLg72NrF1qcsKeVt&_nc_ohc=lQfTCQpiZBwAb4u3Okv&_nc_ht=scontent.falg6-2.fna&oh=00_AfB0u540Fl0jTUtXrO92p0YVJ7PDKKMqufI4bHRjZTv0ug&oe=662874E9"
+            className="rounded-3xl w-full"
+            src="https://lh5.googleusercontent.com/p/AF1QipNjw1uVRhT0wBVp95edtgeYloI-aNK3JdyLn_cs=w427-h240-k-no"
             alt=""
           />
         </div>
       </section>
 
-      <section className="indie-flower-regular flex flex-col justify-between items-center py-4 capitalize px-8 gap-8 lg:flex-row" id="about">
-       <div className="lg:w-1/2 flex flex-col justify-center items-center gap-8">
-       <h1 className="text-5xl">our mission</h1>
-        <p className="text-center lg:text-start">
-          Our mission at Mohamed Lbjaoui High School is to provide a dynamic
-          learning environment that fosters intellectual curiosity, critical
-          thinking, and a passion for lifelong learning. We strive to instill
-          values of integrity, compassion, and resilience in our students,
-          preparing them to become responsible global citizens and leaders of
-          tomorrow.
-        </p>
-       </div>
-       <div>
-        <img className="rounded-3xl p-4" src="https://images.pexels.com/photos/1438081/pexels-photo-1438081.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" />
-       </div>
+      <section
+        className="indie-flower-regular flex flex-col justify-between items-center py-4 capitalize px-8 gap-8 lg:flex-row"
+        id="about"
+      >
+        <div className="lg:w-1/2 flex flex-col justify-center items-center gap-8">
+          <h1 className="text-5xl">our mission</h1>
+          <p className="text-center lg:text-start">
+            Our mission at Mohamed Lbjaoui High School is to provide a dynamic
+            learning environment that fosters intellectual curiosity, critical
+            thinking, and a passion for lifelong learning. We strive to instill
+            values of integrity, compassion, and resilience in our students,
+            preparing them to become responsible global citizens and leaders of
+            tomorrow.
+          </p>
+        </div>
+        <div>
+          <img
+            className="rounded-3xl p-4"
+            src="https://images.pexels.com/photos/1438081/pexels-photo-1438081.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+          />
+        </div>
       </section>
 
       <section
-        className="indie-flower-regular flex flex-col items-center justify-center gap-8 py-4 px-16"
+        className="indie-flower-regular flex flex-col items-center justify-center gap-8 py-4 px-4"
         id="social-media"
       >
-        <h1 className="capitalize text-5xl text-center ">social media</h1>
-        <h3 className="text-3xl">Follow, Like, Share, and Engage!</h3>
+        <h1 className="capitalize text-5xl text-center">social media</h1>
+        <h3 className="text-4xl text-center">
+          Follow, Like, Share, and Engage!
+        </h3>
         <p className="text-2xl text-center px-8">
-
-Join us on social media to stay connected, share your experiences, and be part of our vibrant online community. We look forward to connecting with you!
-
-</p>
+          Join us on social media to stay connected, share your experiences, and
+          be part of our vibrant online community. We look forward to connecting
+          with you!
+        </p>
         <div className="flex gap-4 items-center justify-center">
           <a target="new" href="https://google.com">
             <img
@@ -220,23 +228,103 @@ Join us on social media to stay connected, share your experiences, and be part o
         </div>
       </section>
 
-      <section className="indie-flower-regular py-8 px-8 capitalize" id="school-pics">
+      <section
+        className="indie-flower-regular py-8 px-8 capitalize"
+        id="school-pics"
+      >
         <h1 className="text-5xl text-center mb-8 ">school pics </h1>
-        <p className="text-center text-2xl mb-4">this are some pictures of the high school </p>
+        <p className="text-center text-2xl mb-4">
+          this are some pictures of the high school{" "}
+        </p>
         <div className="flex flex-col lg:flex-row justify-between items-center gap-4 px-8 my-4">
-          <img className="lg:w-4/12 w-full" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=800" alt="school-pic" />
-          <img className="lg:w-4/12 w-full" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=800" alt="school-pic" />
-          <img className="lg:w-4/12 w-full" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=800" alt="school-pic" />
+          <img
+            className="lg:w-4/12 w-full"
+            src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="school-pic"
+          />
+          <img
+            className="lg:w-4/12 w-full"
+            src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="school-pic"
+          />
+          <img
+            className="lg:w-4/12 w-full"
+            src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="school-pic"
+          />
         </div>
         <div className="flex flex-col lg:flex-row justify-between items-center  gap-4 my-8 px-8">
-          <img className="lg:w-4/12 w-full" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=800" alt="school-pic" />
-          <img className="lg:w-4/12 w-full" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=800" alt="school-pic" />
-          <img className="lg:w-4/12 w-full" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=800" alt="school-pic" />
+          <img
+            className="lg:w-4/12 w-full"
+            src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="school-pic"
+          />
+          <img
+            className="lg:w-4/12 w-full"
+            src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="school-pic"
+          />
+          <img
+            className="lg:w-4/12 w-full"
+            src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="school-pic"
+          />
         </div>
-      
       </section>
-      <footer className="bg-black w-full capitalize flex justify-center items-center p-8 ">
-        <h1 className="text-5xl shadows-into-light-regular text-white text-center">this website was developed and coded by :<br /> <a><span className=" px-4 w-full rounded-3xl text-yellow-200 cursor-pointer hover:bg-white hover:text-black">ZIDOUR Djebril</span></a></h1>
+
+      <section className="flex justify-center items-center flex-col gap-8 my-8">
+        <h1 className="capitalize text-5xl ">our location</h1>
+        <div className="w-full flex justify-center items-center px-8 gap-12">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/4284/4284108.png"
+            className="hidden lg:block"
+            alt=""
+          />
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6396.392285143576!2d3.1799559586914063!3d36.717850299999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x128e5192b0000001%3A0xfa0e5c71dc2fdbfe!2z2KvYp9mG2YjZitipINmF2K3ZhdivINmE2KjYrNin2YjZiiAx!5e0!3m2!1sen!2sdz!4v1713636392250!5m2!1sen!2sdz"
+            width="450"
+            height="450"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            className="rounded-3xl border-2 border-black "
+          ></iframe>
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/4284/4284108.png"
+            className="hidden lg:block"
+            alt=""
+          />
+        </div>
+      </section>
+
+      <footer className="bg-black w-full capitalize flex justify-center items-center p-8 flex justify-center items-center flex-col">
+        <h1 className="text-5xl shadows indie-flower-regular text-white text-center">
+          project proposed by:{" "}
+          <span className="text-yellow-400 ">miss Touati</span> <br /> <br />
+          this website was developed and coded by :<br />{" "}
+        </h1>
+
+        <div className="flex justify-center items-center flex-col text-center gap-2 shadows-into-light-regular mt-4 capitalize text-yellow-200">
+          <h1 className=" text-5xl w-full rounded-3xl  cursor-pointer  ">
+            Zidour Djebril
+          </h1>
+          <h1 className=" text-5xl w-full rounded-3xl  cursor-pointer  ">
+            {" "}
+            zouan abd eraouf{" "}
+          </h1>
+          <h1 className=" text-5xl w-full rounded-3xl  cursor-pointer ">
+            {" "}
+            Moualk cyrine
+          </h1>
+          <h1 className=" text-5xl w-full rounded-3xl  cursor-pointer ">
+            {" "}
+            moussouni nassim{" "}
+          </h1>
+          <h1 className="  text-5xl w-full rounded-3xl  cursor-pointer ">
+            {" "}
+            Mdouh ayoub
+          </h1>
+        </div>
       </footer>
     </>
   );
